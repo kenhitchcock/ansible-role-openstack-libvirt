0382b635e61185744082cf67ea648402d0434377
Ken <ken@kenworklaptop.kenlab.local>
2020-02-20 22:04:20 +0000

    Improving the create_instackenv.sh script. Needs to be done in ansible at some stage.

--- files/create_instackenv.sh
+++ files/create_instackenv.sh
@@ -1,25 +1,32 @@
 #!/bin/bash
 
+VHOST="ssh root@192.168.122.1 "
+PROVISIONNETWORK="default"
+
 (
 for i in $(cat /tmp/overcloud-nodes); do
-  virsh dumpxml $i > /tmp/$i.xml
-  mac=$(xmllint --xpath '//mac[1]' /tmp/$i.xml | awk -F= '{ print $2 }' | sed 's!/>$!!')
-  addr=$(vbmc show $i | awk '/address/ {print "\""$4"\"" }')
-  port=$(vbmc show $i | awk '/port/ { print "\""$4"\"" }')
+  addr=$($VHOST vbmc show $i | awk '/address/ {print "\""$4"\"" }')
+  port=$($VHOST vbmc show $i | awk '/port/ { print "\""$4"\"" }')
+  mac="`$VHOST /usr/bin/virsh domiflist $i | grep openstack | awk '{print $5}'`"
 
   cat <<EOF
       {
          "mac": [
-            $mac
+            "$mac"
          ],
          "name": "$i",
          "pm_addr": $addr,
          "pm_port": $port,
-         "pm_password": "password",
-         "pm_type": "pxe_ipmitool",
-         "pm_user": "admin"
+         "pm_password": "$(cat ~/.ssh/id_rsa)",
+         "pm_type": "pxe_ssh",
+         "pm_user": "root"
       }
 EOF
 
 done
-) |  jq -s '{ "nodes": . }' > instackenv.json
+) |  jq -s '{ "ssh-user": "root",
+              "ssh-key": "$(cat ~/.ssh/id_rsa)",
+              "power_manager": "nova.virt.baremetal.virtual_power_driver.VirtualPowerManager",
+              "host-ip": "192.168.122.1",
+              "arch": "x86_64",
+              "nodes": . }' > instackenv.json
